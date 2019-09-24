cfd5b7fc23df8ded6ea2cd6f86e84d54ad8e5af3
Higor Segobia <higor.segobia@gmail.com>
2019-09-24 11:40:22 -0300

    DND REACT

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend'
+
 import GlobalStyles from './styles/global'
+
+
 import Header from './components/header'
 import Board from './components/Board'
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
    <Header/> 
    <Board/>
    <GlobalStyles/>
-  </>);
+  </ DndProvider>
+  );
 }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, {useRef, useContext} from 'react';
+import {useDrag, useDrop} from 'react-dnd';
 
-
+import BoardContext from '../Board/context';
 import { Container, Label } from './styles';
 
-export default function Card({data}) {
+export default function Card({data, index, listIndex}) {
+
+  const Ref = useRef(); 
+
+  const {move} = useContext(BoardContext);
+
+  const [{isDragging}, dragRef] = useDrag({
+    item: {type: 'CARD', index, listIndex},
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    })
+  })
+
+  const [, dropRef] = useDrop({
+      accept: 'CARD',
+      hover(item, monitor){
+       const draggedListIndex = item.listIndex
+
+      const targetIndexList = listIndex;
+
+        const draggedIndex = item.index;
+        const targetIndex = index;
+
+        if(draggedIndex === targetIndex && draggedListIndex === targetIndexList){
+          return;
+        }
+
+        const targetSize = Ref.current.getBoundingClientRect();
+        const targetCenter = (targetSize.botton - targetSize.top) / 2;
+
+        const draggedOffSet = monitor.getClientOffset;
+        const draggedTop = draggedOffSet.y - targetSize.top;
+
+        if(draggedIndex < targetIndex && draggedTop < targetCenter){
+          return;
+        }
+
+        if(draggedIndex > targetIndex && draggedTop > targetCenter){
+          return;
+        }
+        move(draggedListIndex,draggedIndex,targetIndexList, targetIndex);
+        item.index = targetIndex;
+        item.listIndex = targetIndexList;
+      }
+  })
+
+    dragRef(dropRef(Ref));
   return (
-    <Container>
+    <Container ref = {Ref} isDragging={isDragging}>
       {data.labels.map(label => <Label key={label} color={label}/>)}
       <header>
         <Label color='#7159c1'/>
